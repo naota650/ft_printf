084c220de5edd4362b6831d1a9aedf7ea6f43b5d
Luis Martinez <lmartine@e1z3r5p22.42.us.org>
2018-07-03 18:49:27 -0700

    forgot limits.h

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: lmartine <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/04 16:38:53 by lmartine          #+#    #+#             */
-/*   Updated: 2018/07/03 18:35:10 by lmartine         ###   ########.fr       */
+/*   Updated: 2018/07/03 18:49:13 by lmartine         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
+#include <limits.h>
 
 typedef struct	s_variable
 {
